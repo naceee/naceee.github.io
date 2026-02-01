36d8089acd72912ab524800324d020b60a9907a8
Nace Sever <nace.sever@gmail.com>
2026-02-01 22:25:51 +0100

    included point difference

--- projects/tarok/ranking.py
+++ projects/tarok/ranking.py
@@ -3,15 +3,17 @@ import pandas as pd
 
 
 def multiplayer_elo(
-    rank_df, points_df, number_of_games, K=32, tau=128, initial_elo=1500
+    rank_df, points_df, number_of_games, tau=256, initial_elo=1500, beta=1.0
 ):
     """
     Multiplayer Elo with logistic point-difference outcomes.
 
     rank_df   : DataFrame with ranks (1=best, 4=worst, NaN if not played)
     points_df : DataFrame with point scores (same shape as rank_df)
-    K         : Elo update factor
+    number_of_games : list with number of games played in each round
     tau       : score difference scale (larger = softer)
+    initial_elo : starting Elo rating for all players
+    beta      : placement difference weight
     """
 
     players = rank_df.columns
@@ -23,9 +25,10 @@ def multiplayer_elo(
 
         # Active players
         active = rank_row.dropna().index
-        scores = points_df.loc[idx, active]
+        ranks = rank_row.loc[active]
+        points = points_df.loc[idx, active]
 
-        # Expected & actual scores
+        # Expected and actual scores
         actual = {}
         expected = {}
 
@@ -40,11 +43,17 @@ def multiplayer_elo(
 
                 Rj = elo[j]
 
-                # Logistic actual outcome from points
-                d = scores[i] - scores[j]
-                a_ij = 1.0 / (1.0 + np.exp(-d / tau))
+                # Point difference
+                d = points[i] - points[j]
 
-                # Elo expected outcome
+                # Placement difference (positive if i placed better)
+                dr = ranks[j] - ranks[i]
+
+                # Actual outcome (soft win)
+                x = d / tau + beta * dr
+                a_ij = 1.0 / (1.0 + np.exp(-x))
+
+                # Expected outcome (Elo)
                 e_ij = 1.0 / (1.0 + 10 ** ((Rj - Ri) / 400))
 
                 ai_sum += a_ij
@@ -54,9 +63,8 @@ def multiplayer_elo(
             expected[i] = ei_sum / (len(active) - 1)
 
         # Update Elo
-        K_multiplier = number_of_games[idx] / 30
         for i in active:
-            new_elo[i] += K * K_multiplier * (actual[i] - expected[i])
+            new_elo[i] += number_of_games[idx] * (actual[i] - expected[i])
 
         elo = new_elo
         elo_history.append(elo.copy())
